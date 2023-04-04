c526f70917d6e2011df0452227f043f5b18166d1
Austin Hood <austinmhood@live.com>
2023-04-04 16:56:02 -0400

    Made Counselors into a component for use multiple times

---
+++ pages/Counselors.jsx
@@ -0,0 +1,23 @@
+import Footer from "@/components/Footer"
+import HoroAccentLine from "@/components/HoroAccentLine"
+import Navbar from "@/components/NavBar"
+import Counselor from "../components/Counselor"
+
+const Counselors = () => {
+
+    return(
+        <>
+            <Navbar />
+            <div className="flex flex-col items-center bg-primary-green pt-[3%]">
+                <span className="text-4xl text-slate-100 mb-5">Counselors</span>
+                <HoroAccentLine />
+                <Counselor
+                    src='/person3.png'
+                />
+            </div>
+            <Footer />
+        </>
+    )
+} 
+
+export default Counselors
