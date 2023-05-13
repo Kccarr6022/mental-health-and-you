c09d861cf0d6fc20a124cc391a7a46e422a9f9a2
austin.hood <Austinmhood@live.com>
2023-05-13 03:30:49 -0400

    finished non-responseive styles on the couselors page. Still need to add media queries and add functionality to the buttons.

--- pages/Counselors.tsx
+++ pages/Counselors.tsx
@@ -2,15 +2,130 @@ import Footer from "@/components/Footer"
 import HoroAccentLine from "@/components/HoroAccentLine"
 import Navbar from "@/components/NavBar"
 import Counselor from "../components/Counselor"
+import Image from "next/image"
+import person3 from "../public/person3.png"
 
 const Counselors = () => {
     return (
         <div className="overflow-hidden">
             <Navbar />
-            <div className="flex flex-col items-center bg-primary-green pt-[3%]">
-                <span className="text-4xl text-slate-100 mb-5">Counselors</span>
-                <HoroAccentLine />
-                <Counselor src="/person3.png" />
+            <div className="flex flex-col bg-primary-green pt-[3%]">
+                <div className="flex justify-evenly w-full">
+                    <span className="text-lg md:text-5xl text-slate-100 md:mb-5 ">Public Profile</span>
+                    <span className="text-lg md:text-5xl text-slate-100 md:mb-5 ">Current Patients</span>
+                </div>
+                <span className="flex items-center w-full justify-center">
+                    <div className="border rounded-full border-secondary-green w-5 h-5 bg-secondary-green"/>
+                    <hr className="border-2 border-secondary-green min-w-[40%]"></hr>
+                    <div className="border rounded-full border-secondary-green w-5 h-5 bg-secondary-green"/>
+                    <hr className="border-2 border-secondary-green min-w-[40%]"></hr>
+                    <div className="border rounded-full border-secondary-green w-5 h-5 bg-secondary-green"/>
+                </span>
+                <div className="flex mx-[10%] text-lg">
+                    <div className="flex flex-col max-w-[49.8%] mt-8 pl-[10%]">
+                        <div className="flex items-center">
+                            <Image 
+                                src={person3}
+                                alt=''
+                                height={400}
+                                width={200}
+                            />
+                            <div className="ml-5">
+                                <div className="text-slate-100 mb-5 text-lg">
+                                    <span className="font-bold">Name: </span>
+                                    John Smith
+                                </div>
+                                <div className="text-slate-100 mb-5 text-lg">
+                                    <span className="font-bold">Gender: </span>
+                                    Male
+                                </div>
+                                <div className="text-slate-100 mb-5 text-lg">
+                                    <span className="font-bold">Age: </span>
+                                    26
+                                </div>
+                                <div className="text-slate-100 text-lg">
+                                    <span className="font-bold">Ethnicity: </span>
+                                    Caucasian
+                                </div>
+                            </div>
+                        </div>
+                        <div className="text-slate-100 mt-10 max-w-[60%]">
+                            <span className="font-bold">Specialty: </span>
+                            Specialization in so and so type of psychology
+                        </div>
+                        <div className="text-slate-100 mt-10 max-w-[60%]">
+                            <span className="font-bold">Education: </span>
+                            Masters in Psychology in so and so University
+                        </div>
+                        <div className="text-slate-100 mt-10 max-w-[60%]">
+                            <span className="font-bold">Biography: </span>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </div>
+                        <div className="max-w-[50%] flex justify-center">
+                        <button className="text-lg text-slate-100 bg-secondary-green border-0 rounded-lg my-6 p-4 font-bold">EDIT PROFILE</button>
+
+                        </div>
+                    </div>
+                    <div className="inline-block h-[full] min-h-[1em] min-w-[4px] self-stretch bg-secondary-green rounded-sm"></div>
+                    <div className="flex flex-col items-center mt-8">
+                        <div className='flex flex-col text-slate-100 border-4 border-secondary-green rounded-xl p-6 max-w-[75%]'>
+                            <span className="font-bold">FirstName LastName</span>
+                            <div>
+                                <span className="font-bold">Age: </span>
+                                (age)
+                            </div>
+                            <div>
+                                <span className="font-bold">Gender </span>
+                                (Gender)
+                            </div>
+                            <div>
+                                <span className="font-bold">Brief Description: </span>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </div>
+                        </div>
+                        <div className="flex justify-center bg-secondary-green text-slate-100 py-2 px-4 rounded-b-lg">VIEW PROFILE</div>
+                        <div className='flex flex-col text-slate-100 border-4 border-secondary-green rounded-xl p-6 max-w-[75%] mt-8'>
+                            <span className="font-bold">FirstName LastName</span>
+                            <div>
+                                <span className="font-bold">Age: </span>
+                                (age)
+                            </div>
+                            <div>
+                                <span className="font-bold">Gender </span>
+                                (Gender)
+                            </div>
+                            <div>
+                                <span className="font-bold">Brief Description: </span>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </div>
+                        </div>
+                        <div className="flex justify-center bg-secondary-green text-slate-100 py-2 px-4 rounded-b-lg">VIEW PROFILE</div>
+
+                        <div className='flex flex-col text-white border-4 border-secondary-green rounded-xl p-6 max-w-[75%] mt-8'>
+                            <span className="font-bold">FirstName LastName</span>
+                            <div>
+                                <span className="font-bold">Age: </span>
+                                (age)
+                            </div>
+                            <div>
+                                <span className="font-bold">Gender </span>
+                                (Gender)
+                            </div>
+                            <div>
+                                <span className="font-bold">Brief Description: </span>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            </div>
+                            
+                        </div>
+                        <div className="flex justify-center bg-secondary-green text-slate-100 py-2 px-4 rounded-b-lg">VIEW PROFILE</div>
+
+                    </div>
+                    
+                </div>
             </div>
             <Footer />
         </div>
@@ -18,3 +133,6 @@ const Counselors = () => {
 }
 
 export default Counselors
+
+
+
