5f7782f9817784e3480c43f12f0601f351cfe9e0
austin.hood <Austinmhood@live.com>
2023-05-13 21:28:34 -0400

    Made couselors page responsive

--- pages/Counselors.tsx
+++ pages/Counselors.tsx
@@ -1,7 +1,5 @@
 import Footer from "@/components/Footer"
-import HoroAccentLine from "@/components/HoroAccentLine"
 import Navbar from "@/components/NavBar"
-import Counselor from "../components/Counselor"
 import Image from "next/image"
 import person3 from "../public/person3.png"
 
@@ -11,19 +9,19 @@ const Counselors = () => {
             <Navbar />
             <div className="flex flex-col bg-primary-green pt-[3%]">
                 <div className="flex justify-evenly w-full">
-                    <span className="text-lg md:text-5xl text-slate-100 md:mb-5 ">Public Profile</span>
-                    <span className="text-lg md:text-5xl text-slate-100 md:mb-5 ">Current Patients</span>
+                    <span className="text-xl md:text-5xl text-slate-100 md:mb-5 font-bold md:font-normal">Public Profile</span>
+                    <span className="text-xl md:text-5xl text-slate-100 md:mb-5 hidden md:block">Current Patients</span>
                 </div>
                 <span className="flex items-center w-full justify-center">
                     <div className="border rounded-full border-secondary-green w-5 h-5 bg-secondary-green"/>
                     <hr className="border-2 border-secondary-green min-w-[40%]"></hr>
-                    <div className="border rounded-full border-secondary-green w-5 h-5 bg-secondary-green"/>
+                    <div className="border rounded-full border-secondary-green w-5 h-5 bg-secondary-green hidden md:block"/>
                     <hr className="border-2 border-secondary-green min-w-[40%]"></hr>
                     <div className="border rounded-full border-secondary-green w-5 h-5 bg-secondary-green"/>
                 </span>
-                <div className="flex mx-[10%] text-lg">
-                    <div className="flex flex-col max-w-[49.8%] mt-8 pl-[10%]">
-                        <div className="flex items-center">
+                <div className="md:flex mx-[10%] text-lg">
+                    <div className="flex flex-col md:max-w-[49.8%] mt-8 md:pl-[10%] items-center">
+                        <div className="md:flex items-center">
                             <Image 
                                 src={person3}
                                 alt=''
@@ -49,27 +47,33 @@ const Counselors = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="text-slate-100 mt-10 max-w-[60%]">
+                        <div className="text-slate-100 mt-10 md:max-w-[60%]">
                             <span className="font-bold">Specialty: </span>
                             Specialization in so and so type of psychology
                         </div>
-                        <div className="text-slate-100 mt-10 max-w-[60%]">
+                        <div className="text-slate-100 mt-10 md:max-w-[60%]">
                             <span className="font-bold">Education: </span>
                             Masters in Psychology in so and so University
                         </div>
-                        <div className="text-slate-100 mt-10 max-w-[60%]">
+                        <div className="text-slate-100 mt-10 md:max-w-[60%]">
                             <span className="font-bold">Biography: </span>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                         </div>
-                        <div className="max-w-[50%] flex justify-center">
-                        <button className="text-lg text-slate-100 bg-secondary-green border-0 rounded-lg my-6 p-4 font-bold">EDIT PROFILE</button>
-
+                        <div className="md:max-w-[50%] flex justify-center">
+                            <button className="text-lg text-slate-100 bg-secondary-green border-0 rounded-lg my-6 p-4 font-bold">EDIT PROFILE</button>
                         </div>
                     </div>
-                    <div className="inline-block h-[full] min-h-[1em] min-w-[4px] self-stretch bg-secondary-green rounded-sm"></div>
+                    <span className="text-lg md:text-5xl text-slate-100 md:mb-5 md:hidden font-bold md:font-normal w-full flex justify-center">Current Patients</span>
+                    <span className="flex items-center w-full justify-center md:hidden">
+                        <div className="border rounded-full border-secondary-green w-5 h-5 bg-secondary-green"/>
+                        <hr className="border-2 border-secondary-green min-w-[40%]"></hr>
+                        <hr className="border-2 border-secondary-green min-w-[40%]"></hr>
+                        <div className="border rounded-full border-secondary-green w-5 h-5 bg-secondary-green"/>
+                    </span>
+                    <div className="hidden md:inline-block h-[full] min-h-[1em] min-w-[4px] self-stretch bg-secondary-green rounded-sm"></div>
                     <div className="flex flex-col items-center mt-8">
-                        <div className='flex flex-col text-slate-100 border-4 border-secondary-green rounded-xl p-6 max-w-[75%]'>
+                        <div className='flex flex-col text-slate-100 border-4 border-secondary-green rounded-xl p-6 md:max-w-[75%]'>
                             <span className="font-bold">FirstName LastName</span>
                             <div>
                                 <span className="font-bold">Age: </span>
@@ -86,7 +90,7 @@ const Counselors = () => {
                             </div>
                         </div>
                         <div className="flex justify-center bg-secondary-green text-slate-100 py-2 px-4 rounded-b-lg">VIEW PROFILE</div>
-                        <div className='flex flex-col text-slate-100 border-4 border-secondary-green rounded-xl p-6 max-w-[75%] mt-8'>
+                        <div className='flex flex-col text-slate-100 border-4 border-secondary-green rounded-xl p-6 md:max-w-[75%] mt-8'>
                             <span className="font-bold">FirstName LastName</span>
                             <div>
                                 <span className="font-bold">Age: </span>
@@ -104,7 +108,7 @@ const Counselors = () => {
                         </div>
                         <div className="flex justify-center bg-secondary-green text-slate-100 py-2 px-4 rounded-b-lg">VIEW PROFILE</div>
 
-                        <div className='flex flex-col text-white border-4 border-secondary-green rounded-xl p-6 max-w-[75%] mt-8'>
+                        <div className='flex flex-col text-white border-4 border-secondary-green rounded-xl p-6 md:max-w-[75%] mt-8'>
                             <span className="font-bold">FirstName LastName</span>
                             <div>
                                 <span className="font-bold">Age: </span>
