d0abfe49bc5e8d81a5c3a0c304d246dee2f4fb1b
austin.hood <Austinmhood@live.com>
2023-07-04 03:20:53 -0400

    Added new animations, added new icons in footer, restyled navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -47,13 +47,13 @@ const Navbar = () => {
           className={
             !nav
               ? "hidden"
-              : "absolute z-20 mt-[80px] bg-slate-100 text-2xl border-0 rounded-br-2xl text-center text-primary-green min-w-[12%]"
+              : "absolute z-20 mt-[80px] bg-slate-100 text-2xl border-0 rounded-br-2xl text-center text-primary-green min-w-[12%] animate-fadeDown"
           }
         >
-          <li className="p-2 border-2 border-primary-green">
+          <li className="border-2 border-primary-green p-2">
             <Link href="/Home">Home</Link>
           </li>
-          <li className="p-2 border-2 border-primary-green">
+          <li className="border-2 border-primary-green p-2">
             <Link href="/Counselors">Counselors</Link>
           </li>
           <li className="p-2 border-2 border-primary-green">Services</li>
@@ -73,30 +73,30 @@ const Navbar = () => {
     return (
       <div className="h-[100px] bg-slate-100 relative flex w-[100vw] text-primary-green z-50 drop-shadow-lg justify-between ">
         <ul className="flex w-full justify-between items-center">
-          <li className="pl-8 text-primary-green border-0 rounded-full">
+          <li className="pl-14 text-primary-green border-0 rounded-full">
             <Image src={"/logo.png"} height={80} width={80} alt="" />
           </li>
           
         </ul>
 
-        <ul className="flex w-full items-center gap-6 justify-end mr-6 text-lg">
-          <li className="p-2 border-2 rounded-xl border-primary-green">
+        <ul className="flex w-full items-center gap-10 justify-end mr-6 text-xl">
+          <li className="hover:text-shadow-glow transition-all duration-300">
             <Link href="/Home">Home</Link>
           </li>
-          <li className="p-2 border-2 rounded-xl border-primary-green">
+          <li className="hover:text-shadow-glow transition-all duration-300">
             <Link href="/Counselors">Counselors</Link>
           </li>
-          <li className="p-2 border-2 rounded-xl border-primary-green">Services</li>
-          <li className="p-2 border-2 rounded-xl border-primary-green">Booking</li>
-          <li className="p-2 border-2 rounded-xl border-primary-green">Blog</li>
-          <li className="p-2 border-2 rounded-xl border-primary-green">
+          <li className="hover:text-shadow-glow transition-all duration-300">Services</li>
+          <li className="hover:text-shadow-glow transition-all duration-300">Booking</li>
+          <li className="hover:text-shadow-glow transition-all duration-300">Blog</li>
+          <li className="hover:text-shadow-glow transition-all duration-300">
             <Link href="/About">About</Link>
           </li>
-          <li className="p-2 border-2 rounded-xl border-primary-green">FAQ</li>
-          <li className="p-2 border-2 rounded-xl border-primary-green">
+          <li className="hover:text-shadow-glow transition-all duration-300">FAQ</li>
+          <li className="hover:text-shadow-glow transition-all duration-300">
             Contact
           </li>
-          <li className="px-6 font-bold text-primary-green text-2xl border-4 border-primary-green rounded-xl py-4 hover:bg-primary-green hover:text-slate-100 transition-all duration-200">
+          <li className="px-4 font-bold text-primary-green text-2xl border-4 border-primary-green rounded-xl py-4 hover:bg-primary-green hover:text-slate-100 transition-all duration-200">
             <Link href="/SignUp">Sign Up</Link>
           </li>
         </ul>
